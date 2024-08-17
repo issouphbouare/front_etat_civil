6489211f456861329a0c929c38ac2044db49f51b
Mariam9144 <mariamabdoulayemaiga075@gmail.com>
2024-08-17 14:44:13 +0000

    mon push du 17 08 2024 avec le lien du back

--- src/app/services/variable-g.service.ts
+++ src/app/services/variable-g.service.ts
@@ -7,10 +7,10 @@ import { Injectable } from '@angular/core';
 export class VariableGService {
   api: string="http://localhost:8080"
   apiDist1: string="https://62.171.169.168:8080"
-  apiDist: string="https://backetatcivil-production.up.railway.app"
+  apiDist: string="https://backetat-production.up.railway.app"
 
   constructor(private http: HttpClient) { }
   getApi(){
-    return this.api;
+    return this.apiDist;
   }
 }
